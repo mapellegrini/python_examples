4a04f5143ed92e3bd1c0705b420647fd823dda07
Mark Pellegrini <mapellegrini@comcast.net>
2018-05-14 10:19:29 -0400

    Add dates

---
+++ dates.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import datetime 
+from dateutil.parser import parse
+
+
+date1 = datetime.date(2001, 9, 11) 
+date2 = datetime.date(2011, 4, 30) 
+date3 = datetime.date.today()  
+
+datetime1=datetime.datetime(2001, 9, 11, 23, 59, 59)
+datetime2=datetime.datetime.now()
+datetime3=parse("2009-01-05 22:14:39")
+
+delta1=date3-date2
+delta2=date3-date1
+delta3 = datetime.timedelta(days=1, hours=1, microseconds=-1)
+
+print "Dates"
+for date in [date1, date2, date3]:
+  print date
+
+print "\nDatetimes:" 
+for datetime in [datetime1, datetime2, datetime3]:
+  print datetime
+
+print "\nDeltas:"
+for delta in [delta1, delta2, delta3]:
+  print delta
+
+
+
