e5ce33f8b45eb8755d02c02b602f0c894fdbb774
Mark Pellegrini <mapellegrini@comcast.net>
2019-11-16 00:01:00 -0500

    Polish up simpsonpi to make the simpson part more obvious

--- simpsonpi.py
+++ simpsonpi.py
@@ -4,19 +4,22 @@ import numpy
 import argparse
 import math
 
-def f(x):
+def piover4(x):
   return math.sqrt(1-x*x)
+
+def simpson(func, start, end, steps):
+  width = (end-start) / steps
+  xvals = numpy.arange(start, end, width)
+  area = 0.0  
+  for a in range (0, steps-1):
+    area +=  width * (func(xvals[a]) + func(xvals[a+1]))/2 
+  return area 
 
 parser = argparse.ArgumentParser(description='Calculate pi using Simpson\'s method')
 parser.add_argument('-n', type=int, required=False, default=10, help="Number of steps to use")
 args = parser.parse_args()
 
+pi = 4.0 * simpson(piover4, 0.0, 1.0, args.n)
+print "Pi = ", pi
 
-width = 1.0 / args.n
-vals = numpy.arange(0.0, 1.0, width)
-area = 0.0 
 
-for a in range (0, len(vals)-1):
-  area +=  width * (f(vals[a]) + f(vals[a+1]))/2 
-area *= 4 
-print "Pi = ", area 
